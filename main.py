a0b15685b23c853b8f2db77fcfbd502c2cb7cb94
Damian Jaramillo <damianjaramillo@gmail.com>
2024-02-29 15:10:33 -0500

    mi primera calculadora en git

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 def sumar(a, b):
     # aqui se realiza la suma de dos numeros
+    c = a + b
+    return c
 
 def restar(a, b):
     # aqui se realiza la resta de dos numeros
+    c = a - b
+    return c
 
 def multiplicar(a, b):
     # aqui se realiza la multiplicacion de dos numeros
+    c = a * b
+    return c
 
 def dividir(a, b):
     # aqui se realiza la division de dos numeros
+    c = a / b
+    return c
 
 def main():
     num1 = float(input("Ingrese el primer número: "))
     num2 = float(input("Ingrese el segundo número: "))
 
-    print("Suma:", sumar(num1, num2))
+    print("Suma: ", sumar(num1, num2))
     print("Resta:", restar(num1, num2))
     print("Multiplicación:", multiplicar(num1, num2))
     print("División:", dividir(num1, num2))
